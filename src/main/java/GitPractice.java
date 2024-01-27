90fb4a75aa7393807335af10638cfb60f598a308
orunbekova <sedep.orunbekova@gmail.com>
2024-01-27 13:33:33 -0600

    This is our second attempt commit

--- src/main/java/GitPractice.java
+++ src/main/java/GitPractice.java
@@ -1,6 +1,7 @@
 public class GitPractice {
     public static void main(String[] args) {
-        System.out.println("This is first repo");
+        System.out.println("my first message");
+        System.out.println("my first message2");
 
     }
 }
