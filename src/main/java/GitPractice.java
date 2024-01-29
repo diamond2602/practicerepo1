635d3d526579dacbeee17fdb69fa68ef1a06197f
orunbekova <sedep.orunbekova@gmail.com>
2024-01-29 13:31:10 -0600

    Sedep is practicing GitHub

--- src/main/java/GitPractice.java
+++ src/main/java/GitPractice.java
@@ -2,6 +2,7 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("my first message");
         System.out.println("my first message2");
+        System.out.println("Sedep is practicing with gitHub");
 
     }
 }
