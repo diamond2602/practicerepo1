ab9826fc41e9da7253988792eff1816838730a35
diamond2602 <39782754+diamond2602@users.noreply.github.com>
2024-01-29 19:20:55 -0800

    testing after changed passwd github

--- src/main/java/tesfilethree.java
+++ src/main/java/tesfilethree.java
@@ -1,5 +1,6 @@
 public class tesfilethree {
     public static void main(String[] args) {
         System.out.println("third one");
+        System.out.println("testing after changing github passwd");
     }
 }
